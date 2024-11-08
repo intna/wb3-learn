8e7fdf84bffe63562071211e566720c1ff9f0186
cris <intna1027@gmail.com>
2024-11-08 17:39:15 +0800

    coverage,gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,11 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "./tasks/block-number"
 import "@typechain/hardhat"
+import("hardhat-gas-reporter")
+import("dotenv")
+import 'solidity-coverage'
 
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   defaultNetwork: "hardhat",
@@ -11,6 +15,12 @@ const config: HardhatUserConfig = {
       url: "http://127.0.0.1:8545/",
       chainId: 31337
     }
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
   },
 };
 
--- ignition/modules/deploy.ts
+++ ignition/modules/deploy.ts
@@ -1,9 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const SimpleStorageModule = buildModule("SimpleStorageModule", (m) => {
-  // 部署 SimpleStorage 合约
   const simpleStorage = m.contract("SimpleStorage");
-
   return { simpleStorage };
 });
 
